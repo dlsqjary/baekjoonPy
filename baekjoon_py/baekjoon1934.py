99f3611eb17cc51d9930979915627dc8256c08f7
dlsqjary <inpa95@gmail.com>
2021-03-30 14:54:19 +0900

    commit

---
+++ baekjoon_py/baekjoon1934.py
@@ -0,0 +1,13 @@
+n = int(input())
+for _ in range(n):
+    a,b = map(int, input().split())
+    def gcd(x,y):
+        if y == 0:
+            return x
+        else:
+            return gcd(y, x%y)
+
+    def lcm(x,y):
+        return x * y // gcd(x,y)
+
+    print(lcm(a, b))
