6fe10665d55beb504e1853efe615b7e52aa7020c
dlsqjary <inpa95@gmail.com>
2021-04-13 23:20:54 +0900

    다시!!!

---
+++ baekjoon_py/baekjoon1292.py
@@ -0,0 +1,19 @@
+a , b = map(int, input().split())
+num_list = []
+cnt1 = 0
+cnt2 = 1
+sum = 0
+while len(num_list) < 1000 :
+    if cnt1 == cnt2:
+        cnt1 = 0
+        cnt2 += 1
+    num_list.append(cnt2)
+    cnt1 += 1
+
+for i in range(a-1, b):
+    sum += num_list[i]
+
+print(sum)
+"""
+다시
+"""
