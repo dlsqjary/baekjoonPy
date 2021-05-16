abcb4cd97fd86bd24ab6c6a7be5b608a2b0948c0
dlsqjary <inpa95@gmail.com>
2021-05-16 22:39:46 +0900

    처음 작성한 코드에서는 시간초과가 발생했는데 sys 모듈을 통해서 값을 입력받고 출력하면서 시간초과 문제를 해결함. 앞으로 시간이 중요한 문제에선 sys모듈을 사용하는 습관을 가져야겠음.

--- baekjoon_py/baekjoon2751.py
+++ baekjoon_py/baekjoon2751.py
@@ -1,3 +1,15 @@
+import sys  # 시간초과를 방지하기 위해 sys 사용
+N = int(input())
+li = []
+for i in range(N):
+    li.append(int(sys.stdin.readline()))
+
+for i in sorted(li):
+    sys.stdout.write(str(i) + '\n')
+
+
+# 기존에 작성한 코드
+"""
 N = int(input())
 li = []
 for i in range(N):
@@ -7,3 +19,4 @@ for i in range(N):
 li.sort()
 for i in li:
     print(i)
+"""
